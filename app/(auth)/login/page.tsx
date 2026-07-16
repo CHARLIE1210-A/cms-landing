98f7fec62bcb242846271de7c0ffa75773ee8ce6
Ashutosh <ash04singh@gmail.com>
2026-07-16 15:56:03 +0530

    fix suspense issue

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,19 @@
+import { Suspense } from "react"
 import { LoginForm } from "@/app/(auth)/_components/login-form"
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-4xl">
-        <LoginForm />
+        <Suspense fallback={
+          <div className="flex items-center justify-center min-h-[400px]">
+            <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary"></div>
+          </div>
+        }>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   )
 }
+
